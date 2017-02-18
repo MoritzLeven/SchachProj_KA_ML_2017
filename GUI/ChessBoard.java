289c2aaec6ba7b89081f455862e8518e03b8c7dd
Hendrik R <hendrik.reuhl@gmx.de>
2017-02-18 20:08:11 +0100

    Added Chessboard

---
+++ GUI/ChessBoard.java
@@ -0,0 +1,35 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ChessBoard {
+
+    JFrame frame;
+    JPanel Field[][] = new JPanel[8][8];
+
+    public ChessBoard() {
+        frame = new JFrame("Schachbrett");
+        frame.setSize(500, 500);
+        frame.setLayout(new GridLayout(8, 8));
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Field[i][j] = new JPanel();
+
+                if ((i + j) % 2 == 0) {
+                    Field[i][j].setBackground(Color.black);
+                } else {
+                    Field[i][j].setBackground(Color.white);
+                }
+                frame.add(Field[i][j]);
+            }
+        }
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ChessBoard();
+    }
+}
